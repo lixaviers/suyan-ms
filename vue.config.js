1625179d8fd048586e6f53cee7d0bd819931494c
lixavier <xavier.li@stbchina.cn>
2021-02-01 16:43:11 +0800

    feat:page

--- vue.config.js
+++ vue.config.js
@@ -25,16 +25,7 @@ module.exports = {
     //反向代理
     devServer: {
       port: 8081,
-      proxy: {
-          '/api': {
-              target: 'http://localhost:8080', // 域名
-              ws: false, // 是否启用websockets
-              changOrigin: true, // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-              pathRewrite: {
-                  '/api': '',
-              }
-          }
-      }
+      proxy: {}
     },
     pluginOptions: {
       // 第三方插件配置
