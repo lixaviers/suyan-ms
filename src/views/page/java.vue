56ddf65cedd3b04d9670c9aa474b691567a57335
lixavier <xavier.li@stbchina.cn>
2021-02-08 18:07:37 +0800

    feat:page

--- src/views/page/java.vue
+++ src/views/page/java.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="suyan__home">
-        <p>java基础</p>
+        <h1>Java基础<el-button class="suyan__btn__print" type="primary" plain @click="print">打印pdf</el-button></h1><el-divider></el-divider>
+        <p>111111</p>
     </div>
 </template>
 
 <script>
+import Util from '@/libs/util';
 export default {
+    methods: {
+        print() {
+            Util.print();
+        }
+    }
 }
 </script>
