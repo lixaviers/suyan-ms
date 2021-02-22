2f7705b5336a1cde28ae97c37619423349381f22
lixavier <xavier.li@stbchina.cn>
2021-02-22 18:12:42 +0800

    feat:page

---
+++ src/views/page/mysql.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="suyan__home">
+        <h1>MySQL<el-button class="suyan__btn__print" type="primary" plain @click="print">打印pdf</el-button></h1><el-divider></el-divider>
+        <h3>一、常见的查询算法</h3>
+<ol>
+<li>顺序查找</li>
+</ol>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+    </div>
+</template>
+
+<script>
+import Util from '@/libs/util';
+export default {
+    methods: {
+        print() {
+            Util.print();
+        }
+    }
+}
+</script>
