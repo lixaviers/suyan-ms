e615a960b2b41a9afde71dac96391ed2aad0af88
lixavier <xavier.li@stbchina.cn>
2021-03-03 09:11:02 +0800

    feat:page

---
+++ src/views/page/design.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="suyan__home">
+        <h1>设计模式<el-button class="suyan__btn__print" type="primary" plain @click="print">打印pdf</el-button></h1><el-divider></el-divider>
+        <p>设计模式</p>
+    </div>
+</template>
+
+<script>
+import Util from '@/libs/util';
+export default {
+    methods: {
+        print() {
+            Util.print();
+        }
+    }
+}
+</script>
