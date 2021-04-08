66aed43f7b898f3869ded1ff8bc140ff21ef5c89
lixavier <xavier.li@stbchina.cn>
2021-04-08 09:18:59 +0800

    feat:page

--- src/views/page/collection.vue
+++ src/views/page/collection.vue
@@ -3,9 +3,15 @@
         <h1>集合框架<el-button class="suyan__btn__print" type="primary" plain @click="print">打印pdf</el-button><el-button class="suyan__btn__close" plain @click="closeAside"style="margin-right: 20px;">关闭导航</el-button></h1><el-divider></el-divider>
         <h3>一、ArrayList</h3>
 <p>ArrayList相当于一个对象数组，相对于数组来说，数组效率更高，因为数组是连续的存储数据，索引速度非常的快，数组的大小需要提前固定。ArrayList存储的是Object，相当于一个动态的Object []，在插入两种类型的数据时是不会报错的。但是在使用中，很可能会报类型不匹配的错误，所以ArrayList不是类型安全的，ArrayList因为存储的是Object，所以就涉及了装箱和拆箱的问题，在效率上相对数组来说变低了。jdk1.5之后加入了泛型的概念，让ArrayList在使用的时候更加安全了，同时ArrayList大小可以动态的增加，比数组使用起来更加灵活。</p>
+<h4>1.1 ArrayList的动态扩容时如何自动增加的</h4>
+<p>当在ArrayList中增加一个对象时Java会去检查ArrayList以确保已存在的数组中有足够的容量来存储这个新对象(默认为10，最大容量为int上限，减8是为了容错)，如果没有足够容量就新建一个长度更长的数组(原理的1.5倍)，旧的数组就会使用Arrays.copyOf方法被复制到新的数组中去，现有的数组引用指向了新的数组。</p>
+<h4>1.2 为什么ArrayList的增加或删除操作相对来说效率比较低</h4>
+<p>ArrayList在小于扩容容量的情况下其实增加效率是非常高的，在涉及扩容的情况下添加操作效率确实低，删除操作需要移位拷贝，效率是低点。因为ArrayList中增加(扩容)或者是删除元素要调用System.arrayCopy这种效率很低的方法进行处理，所以如果遇到了数据量略大且需要频繁插入或删除的操作效率就比较低了，遇到这种类型场景应该尽可能使用LinkedList进行替代，效率会高一些。</p>
 <h3>二、LinkedList</h3>
 <p>链接列表，相当于一个带链表的List，同时LinkedList不具有线程安全，想要让LinkedList具有线程安全，可用Collections.synchronizedList(new LinkedList())。它和ArrayList在本质上，ArrayList是基于数组的数据结构，而LinkedList是基于链表的数据结构。在源码中，LinkedList定义了一个双向的引用。每一个Node，都有一个前驱和后驱。包装了在开头和结尾进行get、remove、insert方法。</p>
-<p>ArrayList和LinkedList比较：</p>
+<h4>2.1 LinkedList工作原理和实现</h4>
+<p>LinkedList是以双向链表实现，链表无容量限制(但是双向链表本身需要消耗额外的链表指针空间来操作)，其内部主要为first和last两个Node节点，在每次修改列表时用来指引当前双向链表的首位部位，所以LinkedList不仅仅实现了List接口，还实现了Deque双端队列接口(该接口是Queue队列的子接口)，故LinkedList自动具备双端队列的特性，当我们使用下标方式调用列表的get(index)、set(index, e)方法时需要遍历链表将指针移到到位进行访问(会判断index是否大于链表长度的一半，决定是首部遍历还是尾部遍历，访问的复杂度为O(N/2))，无法像ArrayList那样进行随机访问。(如果i&gt;数组大小的一半，会从末尾移起)，只有在链表两头的操作(add()、addFirst()、removeLast()或用在iterator()上的remove()操作)才不需要进行遍历寻找定位。</p>
+<h4>2.2 ArrayList和LinkedList比较</h4>
 <ol>
 <li>是否包装线程安全：ArrayList和LinkedList都是不同步的，也就是不保证线程安全。</li>
 <li>底层数据结构：ArrayList底层使用的是Object数组，LinkedList底层使用的是双向链表数据结构(1.6之前为循环链表，1。7取消了循环)。</li>
